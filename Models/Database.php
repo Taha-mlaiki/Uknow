24996f43db9a32c198dedaac12ae828838d011fb
taha <mlaikitaha29@gmail.com>
2025-01-20 19:49:10 +0100

    Create the User class and connect to the database

---
+++ Models/Database.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Models;
+
+use PDO;
+use PDOException;
+
+class Database
+{
+    private static $host = "localhost";
+    private static $dbName = "uknow";
+    private static $username = "root";
+    private static $password = "";
+    private static  $instance = null;
+
+    public static function getConnection()
+    {
+        if (self::$instance == null) {
+            try {
+                $host = self::$host;
+                $dbName = self::$dbName;
+                $username = self::$username;
+                $password = self::$password;
+
+                $dsn = "mysql:host={$host};dbname={$dbName}";
+                self::$instance = new PDO($dsn, $username, $password);
+            } catch (PDOException $e) {
+                die("Database connection failed: " . $e->getMessage());
+            }
+        }
+        return self::$instance;
+    }
+}
